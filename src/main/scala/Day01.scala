3f87f1655e7cd450a3e70bcd7316d3bd8fe96b20
Eric Tobin <eatobin@gmail.com>
2023-05-26 22:18:20 -0500

    my stuff and works on idea

--- src/main/scala/Day01.scala
+++ src/main/scala/Day01.scala
@@ -1,11 +1,19 @@
 // scala-cli run . --main-class Day01
+import scala.annotation.tailrec
+
 object Day01:
   private def fuel(mass: Int): Int = (mass / 3) - 2
-  private def tsiolkovsky(mass: Int): Int =
-    if mass < 7 then 0 else fuel(mass) + tsiolkovsky(fuel(mass))
+  private def fuelPlusFuel(mass: Int): Int =
+    @tailrec
+    def fuelAccumulator(m: Int, accum: Int): Int =
+      val newGas: Int = fuel(m)
+      if newGas > 0
+      then fuelAccumulator(m = newGas, accum = accum + newGas)
+      else accum
+    fuelAccumulator(m = mass, accum = 0)
 
   def part1(input: Seq[Int]): Int = input.map(fuel).sum
-  def part2(input: Seq[Int]): Int = input.map(tsiolkovsky).sum
+  def part2(input: Seq[Int]): Int = input.map(fuelPlusFuel).sum
 
   def main(args: Array[String]): Unit =
     val data = io.Source
