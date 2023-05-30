1f704dff9a4f9c6f0dcfed0598f52e46c61795d8
Eric Tobin <eatobin@gmail.com>
2023-05-30 15:44:09 -0500

    add tests, bump scala to 3.3.0

--- project.scala
+++ project.scala
@@ -1,2 +1,3 @@
-//> using scala 3.2.2
+//> using scala 3.3.0
 //> using resourceDir ./resources
+//> using test.dep org.scalatest::scalatest::3.2.16
---
+++ src/test/scala/Day02Suite.scala
@@ -0,0 +1,8 @@
+import org.scalatest.funsuite.AnyFunSuite
+
+class Day02Suite extends AnyFunSuite:
+  val sample: Seq[Int] = Seq(1, 9, 10, 3, 2, 3, 11, 0, 99, 30, 40, 50, 0)
+
+  test("Part 1 should handle sample input correctly") {
+    assert(Day02.part1(sample) == 100)
+  }
